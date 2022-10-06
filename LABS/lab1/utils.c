0e39428d934c00da9378ed92847c6942be5f3176
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-10-06 23:51:07 +0100

    Continued solving the Lab1

---
+++ LABS/lab1/utils.c
@@ -0,0 +1,175 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+#define FLAG 0x7E
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
+#define START_STATE 0
+#define FLAG_STATE 1
+#define A_STATE 2
+#define C_STATE 3
+#define BCC_STATE 4
+#define STOP_STATE 5
+
+struct termios oldtio;
+struct termios newtio;
+
+int llopen(char* serialPortName) {
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 1;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    return fd;
+}
+
+
+void ttclose (int fd) {
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+}
+
+/**
+ * @brief 
+ * 
+ * @param fd 
+ * @param SET : 0 - SET command (from the writer) ; 1 - UA command (from the reader)
+ * @return char* 
+ */
+char* read_cmd(int fd, int set) {
+    int state = START_STATE;
+    char curr_char;
+    char packet_A, packet_C;
+    char* ret = malloc(sizeof(char)*2);
+    
+    while (state != STOP_STATE) {
+        int res = read(fd, &curr_char, 1); // Read one character
+        printf("read %x from state %d\n", curr_char, state);
+
+        switch (state)
+        {
+        case START_STATE:
+            if (curr_char == FLAG) {
+                state = FLAG_STATE;
+            }
+            else {
+                state = START_STATE;
+            }
+            break;
+        case FLAG_STATE:
+            packet_A = curr_char; // Receive the byte A, here
+            if (curr_char == A) {
+                state = A_STATE;
+            }
+            else if (curr_char == FLAG_STATE) {
+                state = FLAG_STATE;
+            }
+            else {
+                state = START_STATE;
+            }
+            break;
+        case A_STATE:
+            packet_C = curr_char;// Receive the byte C, here
+            if (set == 1 && curr_char == C_SET) {
+                state = C_STATE;
+            }
+            else if (set == 0 && curr_char == C_UA) {
+                state = C_STATE;
+            }
+            else if (curr_char == FLAG) {
+                state = FLAG_STATE;
+            }
+            else {
+                state = START_STATE;
+            }
+            break;
+        case C_STATE:
+            if (set == 1 && curr_char == (A ^ C_SET)) {
+                state = BCC_STATE;
+            }
+            else if (curr_char == FLAG) {
+                state = FLAG_STATE;
+            }
+            else {
+                state = START_STATE;
+            }
+            break;
+        case BCC_STATE:
+            if (curr_char == FLAG) {
+                // SUCCESS!
+                state = STOP_STATE;
+                ret[0] = packet_A;
+                ret[1] = packet_C;
+                return ret;
+            }
+            else {
+                state = START_STATE;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    return ret;
+}
---
+++ LABS/lab1/write_noncanonical.c
@@ -0,0 +1,222 @@
+// Write to serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include "utils.c"
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+#define FLAG 0x7E
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
+#define BUF_SIZE 256
+
+volatile int STOP = FALSE;
+unsigned char SET[5];
+unsigned char UA[5];
+int fd; // serial file descriptor
+
+void prepareSet() {
+    SET[0] = FLAG;
+    SET[1] = A;
+    SET[2] = C_SET;
+    SET[3] = A ^ C_SET;
+    SET[4] = FLAG;
+}
+
+void prepareUA() {
+    UA[0] = FLAG;
+    UA[1] = A;
+    UA[2] = C_UA;
+    UA[3] = A ^ C_UA;
+    UA[4] = FLAG;
+}
+
+void sendSet() {
+    int sentBytes = 0;
+    sentBytes = write(fd, SET, 5);
+    printf("Sent bytes: %d ", sentBytes);
+}
+
+void receiveUA(int fd) {
+    int state = 0;
+    unsigned char ch;
+
+    while (state != 5) { // While it doesn't get to the end of the state macgine
+        read(fd, &ch, 1); // Read one char
+        switch (ch)
+        {
+        case 0: // Start node (waiting fot the FLAG)
+            if (ch == UA[0]) {
+                state = 1; // Go to the next state
+            } // else { stay in the same state }
+            break;
+        case 1: // State Flag RCV
+            if (ch == UA[1]) {
+                state = 2; // Go to the next state
+            }
+            else if (ch == UA[0]) {
+                state = 1; // Stays on the same state
+            }
+            else {
+                state = 0; // other character received goes to the initial state
+            }
+            break;
+        case 2: // State A RCV
+            if (ch == UA[2]) {
+                state = 3; // Go to the next state
+            }
+            else if (ch == UA[0]) {
+                state = 1;
+            }
+            else {
+                state = 0;
+            }
+            break;
+        case 3: // State C RCV
+            if (ch == UA[3]) {
+                state = 4; // Go to the next state
+            }
+            else if (ch == UA[0]) {
+                state = 1;
+            }
+            else {
+                state = 0;
+            }
+            break;
+        case 4: // State BCC_OK
+            if (ch == UA[4]) {
+                state = 5; // Go to the final state
+            }
+            else {
+                state = 0;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing, and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    fd = open(serialPortName, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 1;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // In non-canonical mode, '\n' does not end the writing.
+    // Test this condition by placing a '\n' in the middle of the buffer.
+    // The whole buffer must be sent even with the '\n'.
+    // buf[5] = '\n';
+
+    prepareSet(); // prepare the SET array
+    
+    sendSet(); // Send SET to the writer
+    
+    sleep(1); // wait until all the bytes have been written to the reader
+
+
+
+    // Wait until all bytes have been written to the serial port
+    //sleep(1);
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+
+
+
+    // If I want to read the sentence the reader sent me
+    // while (STOP == FALSE)
+    // {
+    //     // Returns after 5 chars have been input
+    //     int bytes = read(fd, buf, BUF_SIZE);
+
+    //     printf(":%s:%d\n", buf, bytes);
+    //     if (buf[bytes] == '\0')
+    //         STOP = TRUE;
+    // }
+
+    close(fd);
+
+    return 0;
+}
